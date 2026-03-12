200b2a968e7ead18f236d62956e6c2704dde25d6
khushila <khushilachamariya@gmail.com>
2026-03-12 16:51:05 +0530

    update: change api url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8787',
+        target: 'https://mib-backend.mibforindia.workers.dev/',
         changeOrigin: true,
         secure: false,
       }
